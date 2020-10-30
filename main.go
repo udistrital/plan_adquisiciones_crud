d35c52a54e7099df8016049cb18943b8afc1a59e
Jeisson P <jeisson005@hotmail.com>
2020-10-30 15:13:49 -0500

    feat: Configuracion CORS #3

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/orm"
+	"github.com/astaxie/beego/plugins/cors"
 	_ "github.com/lib/pq"
 )
 
@@ -14,6 +15,18 @@ func main() {
 		beego.BConfig.WebConfig.DirectoryIndex = true
 		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
 	}
+	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{"PUT", "PATCH", "GET", "POST", "OPTIONS", "DELETE"},
+		AllowHeaders: []string{"Origin", "x-requested-with",
+		  "content-type",
+		  "accept",
+		  "origin",
+		  "authorization",
+		  "x-csrftoken"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	  }))
 	beego.Run()
 }
 
