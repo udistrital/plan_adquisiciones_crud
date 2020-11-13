b619c690be8eef5052caa39c694d64b8933da65b
Jeisson P <jeisson005@hotmail.com>
2020-11-13 16:25:21 -0500

    feat: Consulta a base de datos mongo #10

--- main.go
+++ main.go
@@ -4,14 +4,31 @@ import (
 	_ "github.com/udistrital/plan_adquisiciones_crud/routers"
 	apistatus "github.com/udistrital/utils_oas/apiStatusLib"
 	"github.com/udistrital/utils_oas/customerror"
-
+	dbMongoManager "github.com/udistrital/plan_adquisiciones_crud/database"
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/orm"
 	"github.com/astaxie/beego/plugins/cors"
 	_ "github.com/lib/pq"
+
+	"fmt"
+	"encoding/json"
 )
 
 func main() {
+	dbMongoManager.InitDB(beego.AppConfig.String("mongoHost"),
+		beego.AppConfig.String("mongoPort"),
+		beego.AppConfig.String("mongoUser"),
+		beego.AppConfig.String("mongoPass"),
+		beego.AppConfig.String("mongoAuth"),
+		beego.AppConfig.String("mongodb"),
+	)
+	var dat, _ = dbMongoManager.GetDatabase();
+	var coll = dat.Collection("plan_adquisiciones_crud_mongo");
+	fmt.Println("hello world-------------------------------------------");
+	fmt.Println(coll);
+	fmt.Println("hello world-------------------------------------------");
+	fmt.Println(json.Marshal(coll));
+	fmt.Println("hello world-------------------------------------------");
 	orm.RegisterDataBase("default", "postgres", "postgres://"+beego.AppConfig.String("PGuser")+":"+beego.AppConfig.String("PGpass")+"@"+beego.AppConfig.String("PGhost")+"/"+beego.AppConfig.String("PGdb")+"?sslmode=disable&search_path"+"="+beego.AppConfig.String("PGschemas"))
 	if beego.BConfig.RunMode == "dev" {
 		beego.BConfig.WebConfig.DirectoryIndex = true
