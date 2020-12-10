045a9535228d8d8027f0867245a4d712bc06a83e
jjvargass <jjvargass@udistrital.edu.co>
2020-12-09 19:13:24 -0500

    devops:ajustes lineamiento variables de entorno

--- main.go
+++ main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-	_ "github.com/udistrital/plan_adquisiciones_crud/routers"
-	apistatus "github.com/udistrital/utils_oas/apiStatusLib"
-	"github.com/udistrital/utils_oas/customerror"
-	dbMongoManager "github.com/udistrital/plan_adquisiciones_crud/database"
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/orm"
 	"github.com/astaxie/beego/plugins/cors"
 	_ "github.com/lib/pq"
+	dbMongoManager "github.com/udistrital/plan_adquisiciones_crud/database"
+	_ "github.com/udistrital/plan_adquisiciones_crud/routers"
+	apistatus "github.com/udistrital/utils_oas/apiStatusLib"
+	"github.com/udistrital/utils_oas/customerror"
 )
 
 func main() {
@@ -20,7 +20,15 @@ func main() {
 		beego.AppConfig.String("mongodb"),
 		beego.AppConfig.String("mongoCollection"),
 	)
-	orm.RegisterDataBase("default", "postgres", "postgres://"+beego.AppConfig.String("PGuser")+":"+beego.AppConfig.String("PGpass")+"@"+beego.AppConfig.String("PGhost")+"/"+beego.AppConfig.String("PGdb")+"?sslmode=disable&search_path"+"="+beego.AppConfig.String("PGschemas"))
+
+	orm.RegisterDataBase("default", "postgres", "postgres://"+
+		beego.AppConfig.String("PGuser")+":"+
+		beego.AppConfig.String("PGpass")+"@"+
+		beego.AppConfig.String("PGhost")+
+		beego.AppConfig.String("PGport")+"/"+
+		beego.AppConfig.String("PGdb")+"?sslmode=disable&search_path="+
+		beego.AppConfig.String("PGschema")+"")
+
 	if beego.BConfig.RunMode == "dev" {
 		beego.BConfig.WebConfig.DirectoryIndex = true
 		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
