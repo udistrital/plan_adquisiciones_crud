eb01bda76d204fb5800095751280588e022034fd
brayanpasa99 <brayanpasa99@hotmail.com>
2022-03-02 19:23:52 -0500

    refactor: Migraciones de cambio de valor de moneda (Lineamientos)
    Resuelve: udistrital/financiera_documentacion#411

---
+++ database/migrations/20220302_190101_cambiar_tipo_dato_valor_integer_to_numeric.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+
+	"github.com/astaxie/beego/migration"
+)
+
+// DO NOT MODIFY
+type CambiarTipoDatoValorIntegerToNumeric_20220302_190101 struct {
+	migration.Migration
+}
+
+// DO NOT MODIFY
+func init() {
+	m := &CambiarTipoDatoValorIntegerToNumeric_20220302_190101{}
+	m.Created = "20220302_190101"
+
+	migration.Register("CambiarTipoDatoValorIntegerToNumeric_20220302_190101", m)
+}
+
+// Run the migrations
+func (m *CambiarTipoDatoValorIntegerToNumeric_20220302_190101) Up() {
+	// use m.SQL("CREATE TABLE ...") to make schema update
+	file, err := ioutil.ReadFile("../scripts/cambiar_tipo_dato_valor_integer_to_numeric.up.sql")
+
+	if err != nil {
+		// handle error
+		fmt.Println(err)
+	}
+
+	requests := strings.Split(string(file), ";")
+
+	for _, request := range requests {
+		fmt.Println(request)
+		m.SQL(request)
+		// do whatever you need with result and error
+	}
+
+}
+
+// Reverse the migrations
+func (m *CambiarTipoDatoValorIntegerToNumeric_20220302_190101) Down() {
+	// use m.SQL("DROP TABLE ...") to reverse schema update
+	file, err := ioutil.ReadFile("../scripts/cambiar_tipo_dato_valor_integer_to_numeric.down.sql")
+
+	if err != nil {
+		// handle error
+		fmt.Println(err)
+	}
+
+	requests := strings.Split(string(file), ";")
+
+	for _, request := range requests {
+		fmt.Println(request)
+		m.SQL(request)
+		// do whatever you need with result and error
+	}
+}
